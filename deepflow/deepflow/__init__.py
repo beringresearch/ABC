28c471c36d32338d7e9b37a6345a8182529d5862
Ignat Drozdov <idrozdov@beringresearch.com>
2017-02-23 12:33:29 +0000

    added support for multiple command line arguments

--- deepflow/deepflow/__init__.py
+++ deepflow/deepflow/__init__.py
@@ -4,8 +4,8 @@ from . import autoencoder
 
 def main():
     markers, input_files = argparser.parse_input_filepaths(sys.argv)
-    output_folder = argparser.create_output_folder()
-    autoencoder.run(markers, input_files, output_folder)
+    images_folder, logs_folder = argparser.create_output_folder()
+    autoencoder.run(markers, input_files, images_folder, logs_folder)
 
 if __name__ == '__main__':
     main()
