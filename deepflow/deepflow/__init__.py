bf29926c9ce36917109c066a843118c5d720ba3d
Ignat Drozdov <idrozdov@beringresearch.com>
2017-02-24 16:45:17 +0000

    add skiprow argument to deepflow cli

--- deepflow/deepflow/__init__.py
+++ deepflow/deepflow/__init__.py
@@ -3,9 +3,9 @@ from . import argparser
 from . import autoencoder
 
 def main():
-    markers, input_files = argparser.parse_input_filepaths(sys.argv)
+    markers, input_files, nskip = argparser.parse_input_filepaths(sys.argv)
     images_folder, logs_folder = argparser.create_output_folder()
-    autoencoder.run(markers, input_files, images_folder, logs_folder)
+    autoencoder.run(markers, input_files, nskip, images_folder, logs_folder)
 
 if __name__ == '__main__':
     main()
