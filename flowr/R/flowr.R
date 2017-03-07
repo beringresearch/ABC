8962fc6dbb6ff010b5d2692d1326d84ddc5df1f8
Ignat Drozdov <idrozdov@beringresearch.com>
2017-03-07 14:11:09 +0000

    first

---
+++ flowr/R/flowr.R
@@ -0,0 +1,12 @@
+#' Execute the flowr configuration file
+#' 
+#' @param config 	character string containing a path 
+#' 			to the flowr configuration file
+#' @export
+#'
+
+flowr <- function(config){
+	params <- yaml::yaml.load_file(config)
+
+
+}
