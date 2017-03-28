ebb4337bd09d8048d2e1af8f68174e1a4cf90392
Ignat Drozdov <idrozdov@beringresearch.com>
2017-03-28 11:46:16 +0100

    unload attached packages

--- virtualenv/R/ve_deactivate.R
+++ virtualenv/R/ve_deactivate.R
@@ -5,5 +5,11 @@
 #' @export
 
 ve_deactivate <- function(){
+
+	session <- sessionInfo()
+	loadedPkgs <- names(session$otherPkgs)
+	pkg <- paste("package:", loadedPkgs, sep="")
+	lapply(pkg, detach, character.only = TRUE, unload = TRUE)
+
 	.libPaths(Sys.getenv("R_LIBS_USER"))
 }
