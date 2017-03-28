5ecefb6135bd7299691b99676e22e5220adc6544
Ignat Drozdov <idrozdov@beringresearch.com>
2017-03-28 10:04:35 +0100

    dont copy yaml file

--- virtualenv/R/ve_new.R
+++ virtualenv/R/ve_new.R
@@ -21,14 +21,13 @@ ve_new <- function(config_path){
 	       )
 
 	# Copy dependencies yaml to environment directory for future use
-	out <- file.copy(from=config_path, to=env_dir)
+	#out <- file.copy(from=config_path, to=env_dir)
 	
 	# Cycle through repositories, installing specific packages
 	repositories <- names(config$Repository)
 
 	for (n in repositories){
 		if (n=="CRAN"){
-
 			task <- config$Repository[[n]]$Packages
 			pkg <- names(task)
 			version <- as.vector(unlist(task))
@@ -39,7 +38,6 @@ ve_new <- function(config_path){
 			install.packages(deps, lib=env_dir)			
 		}
 	}
-
 }
 
 get_package_url <- function(pkg, version){
