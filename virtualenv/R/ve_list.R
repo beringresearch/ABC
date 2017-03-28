85ab84e75b3496a5c8f8a39974dab10fd826b998
Ignat Drozdov <idrozdov@beringresearch.com>
2017-03-28 09:26:27 +0100

    pretty output for environment listings

--- virtualenv/R/ve_list.R
+++ virtualenv/R/ve_list.R
@@ -13,16 +13,27 @@ ve_list <- function(){
 	envs <- dir(ve_dir, full.names = FALSE)
 
 	if (length(envs) > 0){
-		res <- data.frame(Environment=NA, Size=NA, Owner=NA)
+		res <- data.frame(Environment=NA, Size=NA, Owner=NA, Libraries=NA)
 
 		for(n in 1:length(envs)){
 			info <- file.info(file.path(ve_dir, envs[n]))
 			res[n, "Environment"] <- envs[n]
 			res[n, "Size"] <- info$size	
 			res[n, "Owner"] <- info$uname
+			res[n, "Libraries"] <- paste0(strtrim(
+						      paste0(environment_libs(envs[n]),
+							     collapse=", "),
+					    		25), "...")
 		}	
 		return(res)	
 	}else{
 		message("No R environments. Run ve_new() to create one.")
 	}
+}
+
+#' Show environment libraries
+environment_libs <- function(name){
+	HOME <- Sys.getenv("HOME")
+	ve_dir <- file.path(HOME, ".renvironments", name)
+	setdiff(dir(ve_dir, full.names = FALSE), "environment.yaml")
 }
