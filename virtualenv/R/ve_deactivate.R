3066abdefcd0894ca89e7bf7a7ca0243c146027b
Ignat Drozdov <idrozdov@beringresearch.com>
2017-03-28 12:19:15 +0100

    unload all attached packages prior to deactivation

--- virtualenv/R/ve_deactivate.R
+++ virtualenv/R/ve_deactivate.R
@@ -1,15 +1,19 @@
 #' Deactivate current virtual environment
 #'
-#' Upon deactivation, R environment is set to default package environemnt
+#' Upon deactivation, R environment is set to default package environemnt.
+#' All attached packages are unloaded
 #'
 #' @export
 
 ve_deactivate <- function(){
 
 	session <- sessionInfo()
-	loadedPkgs <- names(session$otherPkgs)
-	pkg <- paste("package:", loadedPkgs, sep="")
-	lapply(pkg, detach, character.only = TRUE, unload = TRUE)
+	loadedPkgs <- c(names(session$otherPkgs))
+	
+	if (!is.null(loadedPkgs)){
+		loadedPkgs <- paste("package", loadedPkgs, sep=":")
+		lapply(loadedPkgs, detach, unload=TRUE, character.only=TRUE)
+	}
 
 	.libPaths(Sys.getenv("R_LIBS_USER"))
 }
