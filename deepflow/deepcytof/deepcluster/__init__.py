d9a54c3e18189cac687f460254789dc4e1ebda13
Ignat Drozdov <idrozdov@beringresearch.com>
2017-04-15 11:32:00 +0100

    added unsupervised neural clustering

--- deepflow/deepcytof/deepcluster/__init__.py
+++ deepflow/deepcytof/deepcluster/__init__.py
@@ -3,9 +3,9 @@ from . import argparser
 from . import cluster
 
 def main():
-    cluster_size, input_files = argparser.parse_input_filepaths(sys.arg)
+    markers, files, weights, cluster_size, nskip = argparser.parse_input_filepaths(sys.argv)
     images_folder, logs_folder = argparser.create_output_folder()
-    cluster.run(input_files, cluster_size, images_folder, logs_folder)
+    cluster.run(markers, files, weights, images_folder, logs_folder, cluster_size,  nskip)
 
 if __name__ == '__main__':
     main()
