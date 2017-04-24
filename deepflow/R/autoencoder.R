c43ef5ce47a1b714c1b51e12604ea9546b43cf5c
Ignat Drozdov <idrozdov@beringresearch.com>
2017-04-24 05:25:47 +0100

    ensure encodings are persistent through time

--- deepflow/R/autoencoder.R
+++ deepflow/R/autoencoder.R
@@ -15,29 +15,39 @@ autoencoder <- function(x, hidden = c(150, 150, 500, 4),
 	
 	# Encoder
 	encoder <- Sequential()
-	encoder$add(Dense(units=hidden[1], activation="linear", input_shape=dim(x)[2]))
+	encoder$add(Dense(units=hidden[1],
+			  activation="linear",
+			  input_shape=dim(x)[2],
+			  kernel_initializer=glorot_uniform(seed=1234)))
 	for (n in 2:length(hidden)){
 		encoder$add(Dense(units=hidden[n],
-				activation=activation[n]))
+				activation=activation[n],
+				kernel_initializer=glorot_uniform(seed=1234)))
 	}
 
 	# Decoder
 	decoder <- Sequential()
 	decoder$add(Dense(units=hidden[length(hidden)-1],
 			  activation=activation[length(hidden)-1],
-			  input_shape=hidden[length(hidden)]))
+			  input_shape=hidden[length(hidden)],
+			  kernel_initializer=glorot_uniform(seed=1234)))
 	for (n in length(hidden)-1 : 1){
 		decoder$add(Dense(units=hidden[n],
-				  activation=activation[n]))
+				  activation=activation[n],
+				  kernel_initializer=glorot_uniform(seed=1234)))
 	}
-	decoder$add(Dense(units=dim(x)[2]))
+	decoder$add(Dense(units=dim(x)[2],
+			  kernel_initializer=glorot_uniform(seed=1234)))
 
 	autoencoder <- Sequential()
 	autoencoder$add(encoder)
 	autoencoder$add(decoder)
 
 	keras_compile(autoencoder, loss = "mse", optimizer=Adam())
-	keras_fit(autoencoder, x, x, epochs = epochs, verbose=1)
+	keras_fit(autoencoder, x, x,
+		  epochs = epochs,
+		  shuffle = FALSE,
+		  verbose=1)
 
 	return(list(encoder=encoder, decoder=decoder, autoencoder=autoencoder))
 
