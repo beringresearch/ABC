f46a114949c8c8c61793f9eb9e0654be04890f34
Ignat Drozdov <idrozdov@beringresearch.com>
2017-05-10 15:43:19 +0100

    add support for regression and minor bug fixes

--- quickpeak/R/qpeak.R
+++ quickpeak/R/qpeak.R
@@ -14,38 +14,48 @@
 qpeak <- function(model, X, feature, which.class=1L, predict, ...){
 
 	if (! feature %in% colnames(X))
-		stop(paste0(feature, " not found oin column names of the training set."))
+		stop(paste0(feature, " not found in column names of the training set."))
 	
 	ix <- match(feature, colnames(X))	
-	x_new <- X[,-ix]
-	feature_vector <- X[,ix]
+	x_new  <- X[,-ix]
 	
-	is.factor <- sapply(x_new, class) == "factor"
+        newdata	<- data.frame(X[,ix])
+	colnames(newdata) <- feature
+	
+	is_factor <- sapply(x_new, class) == "factor"
 
-	if (sum(!is.factor) > 0)
-		x_new[, !is.factor] <- apply(x_new[, !is.factor], 2, replace_continuous)
+	if (sum(!is_factor) > 0){
+		numeric_df <- apply(data.frame(x_new[, !is_factor]), 2, replace_continuous)
+		colnames(numeric_df) <- colnames(x_new)[!is_factor]
+		newdata <- data.frame(newdata, numeric_df)
+	}
 
-	if (sum(is.factor) > 0)
-		x_new[, is.factor] <- apply(x_new[, is.factor], 2, replace_factor)
+	if (sum(is_factor) > 0){
+		factor_df <- apply(data.frame(x_new[, is_factor]), 2, replace_factor)
+		colnames(factor_df) <- colnames(x_new)[is_factor]
+		newdata <- data.frame(newdata, factor_df)
+	}
 
-	cnames <- c(colnames(X)[ix], colnames(x_new))
-	x_new <- cbind(feature_vector, x_new)
-	colnames(x_new) <- cnames
 	
 	# Remove duplicated features if predictor is a factor.
 	# Only predict using unique factor levels	
-	if (is.factor(feature_vector))
-		x_new <- x_new[!duplicated(x_new),]
+	if (is.factor(newdata[,feature]))
+		newdata <- newdata[!duplicated(newdata),]
 
-	yh <- predict(model, x_new, ...)
+	yh <- predict(model, newdata, ...)
 	
 	if (!is.numeric(yh))
 		stop("predict must return numeric matrix of class probabilities.")
 
-	odds <- yh[,which.class]/(1-yh[,which.class])
+	if (!is.null(dim(yh))){
+		# This is a classification problem
+		odds <- yh[,which.class]/(1-yh[,which.class])
+	}else{
+		odds <- yh
+	}
 
-	out <- data.frame(x=x_new[,1], yhat=odds)
-	colnames(out) <- c(feature, "odds")
+	out <- data.frame(newdata[,feature], yhat=odds)
+	colnames(out) <- c(feature, "yhat")
 
 	return(out)
 }
