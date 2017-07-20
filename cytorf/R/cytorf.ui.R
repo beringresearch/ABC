3790639145f48a9bbbc40dd5bc87e8577fd9cb99
Ignat Drozdov <idrozdov@beringresearch.com>
2017-07-20 17:57:34 +0100

    add interactive cluster visualisations

--- cytorf/R/cytorf.ui.R
+++ cytorf/R/cytorf.ui.R
@@ -1,65 +1,110 @@
 #' Shiny interface to CytoRF clustering algorithm
 #'
 #' @import ggplot2
-#' @import largeVis
-#' @import shiny shinyFiles flowCore
+#' @import Rtsne
+#' @import shiny shinyFiles flowCore RColorBrewer
 #' @export
 
 cytorf.ui <- function(){	
 
-	global = reactiveValues(fcs_raw = NULL)
+	global = reactiveValues(fcs_raw = NULL,
+				X = NULL,
+				g = NULL,
+				coords = NULL)
 
 	app <- list(	
 		    ui=fluidPage(
 				 titlePanel("CytoRF"),
 
-				 sidebarPanel(  
-					      h3("Clustering Options"), 
+				 sidebarLayout(position="left",
+
+				 sidebarPanel(width = 3,   
 					      numericInput("ntrees", "Number of Trees",
 							   125, min = 2, max = 1000),
 					      numericInput("scale", "Cluster Granularity",
 							   7, min=1, max=25),
 					      numericInput("seed", "Random Seed",
-							   12345, min=1, max=Inf)
+							   12345, min=1, max=Inf),
+
+					      hr(),
+					      htmlOutput("visualise_channel")
+
 					      ),
 
-				 mainPanel(
+				 mainPanel(width = 9,
 					   tabsetPanel(type = "tabs",
-						       tabPanel("Setup Clustering Analysis",
+						       tabPanel("Setup Your Analysis",
 								br(),
-								h3("Step 1: Chose Project Files"),
+								h4("Step 1: Chose Project Files"),
+								div(style="display: inline-block;vertical-align:top; width: 700px;",
+
 								shinyFilesButton("files",
 							     		label="Chose Project Files",
 							     		title="Select Project Files",	
-							     		multiple=TRUE),
+							     		multiple=TRUE)
+								),
 					      			
+								div(style="display: inline-block;vertical-align:top; width: 200px;",
+								helpText("Click on the button to navigate to a folder that contains your FCS files. Select one or more files to begin your analysis.")
+								),
+
+
 								hr(),
-								h3("Step 2: Select Channels of Interest"),
-								div(style="display: inline-block;vertical-align:top; width: 250px;",
+								h4("Step 2: Select Channels of Interest"),
+								div(style="display: inline-block;vertical-align:top; width: 350px;",
 								    htmlOutput("file_list")
 								),
-								div(style="display: inline-block;vertical-align:top; width: 250px;",
+								div(style="display: inline-block;vertical-align:top; width: 350px;",
 								    htmlOutput("channel_list")
 								),
-								div(style="display: inline-block;vertical-align:top; width: 250px;",
-								    htmlOutput("selected_channels")
+								div(style="display: inline-block;vertical-align:top; width: 200px;",
+								    helpText("Select two or more items from the Available Channels list. Selected channels will be used to cluster your FCS data and overlay it with relevant expression profiles.")
 								),
+
+								
 								hr(),
-								h3("Step 3: Cluster"),
-								htmlOutput("run_analysis_subsample"),
-								htmlOutput("run_analysis_run"),
+								h4("Step 3: Cluster"),
+								div(style="display: inline-block;vertical-align:top; width: 700px;",
+								    htmlOutput("run_analysis_subsample"),
+								    htmlOutput("run_analysis_run")),
+								div(style="display: inline-block;vertical-align:top; width: 200px;",
+								    helpText("Running time is directly proportional to number of events from each file. To quickly assess clustering output, select a small subsample.")),
 
 								hr()
 								),
 						       tabPanel("Results",
-								verbatimTextOutput("analysis_summary")
+								br(),
+								div(style="display: inline-block;vertical-align:top; width: 500px;",
+
+								verbatimTextOutput("cluster_info",
+										   placeholder = TRUE)),
+								div(style="display: inline-block;vertical-align:top; width: 500px;",
+
+								verbatimTextOutput("analysis_summary",
+										   placeholder = TRUE)),
+
+
+								br(),	
+
+								div(style="display: inline-block;vertical-align:top; width: 500px;",
+								plotOutput("plot_cluster",
+									   click = "plot_click")
 								),
+								div(style="display: inline-block;vertical-align:top; width: 500px;",
+								plotOutput("plot_expression",
+									   click = "plot_click")
+								),
+								hr()	
+								),
+
 						       tabPanel("Help"))
 
 					  )
+				 )
 				 ),
 
 	server=function(input, output){
+	
 		# File Selection
 		shinyFileChoose(input, 'files',
 				roots=c(home=Sys.getenv("HOME")), 
@@ -102,15 +147,26 @@ cytorf.ui <- function(){
 									       1000, min=10, max=Inf)})
 							  output$run_analysis_run <- renderUI({
 								  actionButton("run", "Run Clustering")})
-					     })
+							  
+							  # Show Channel List in Results section
+							  output$visualise_channel <- renderUI({
+								  selectInput("visualise_channel",
+									      "Visualise Channel",
+									      choices = input$channel_list,
+									      multiple = FALSE)})
+					})
 				})
 
 		# Run Analysis
 		observeEvent(input$run, {
 				     fcs <- fsApply(global$fcs_raw, function(x, cofactor=5){
+
 							    set.seed(input$seed)
+							    nevents <- input$nevents
+							    if (nevents > nrow(x)) nevents <- nrow(x)
 							    subsample <- sample(1:nrow(x),
-									   input$nevents, replace=FALSE)
+										nevents, replace=FALSE)
+
 							    colnames(x) <- global$fcs_raw@colnames
 							    
 							    expr <- exprs(x)
@@ -120,17 +176,95 @@ cytorf.ui <- function(){
 							    exprs(x) <- expr
 							    x})
 
-				     X <- fsApply(fcs, exprs)
-				     g <- cytorf(X, num.trees=input$ntrees,
+				     global$X <- fsApply(fcs, exprs)
+				     global$X <- global$X[!duplicated(global$X),]
+				     global$g <- cytorf(global$X, num.trees=input$ntrees,
 						 scale=input$scale,
 						 seed=input$seed)
-				     nclusters <- length(unique(g))
-				     echo <- paste0("Total number of clusters: ", nclusters, "\n")
+				     nclusters <- length(unique(global$g))
+				     echo <- paste0("Number of clusters: ", nclusters, "\n",
+						    "Number of events: ", nrow(global$X))
 
 				     output$analysis_summary <- renderText({echo})
+				     
+				     if (ncol(global$X) == 2){
+					     	global$coords <- global$X
+				     } else {
+					     	tsne <- Rtsne(global$X)
+					     	global$coords <- tsne$Y[,1:2]
+					     	colnames(global$coords) <- c("viSNE.1", "viSNE.2")
+				     }
+				     
+		
+		# Render Clustering Plot
+		output$plot_cluster <- renderPlot({
+			getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+			colorCount <- length(unique(global$g))
+	
+			df <- data.frame(global$coords)
+			df$Gates <- as.factor(global$g)
+			ggplot(df, aes(x=df[,1], y=df[,2], color=Gates)) + geom_point() +
+			xlab(colnames(global$coords)[1]) +
+			ylab(colnames(global$coords)[2]) +
+			scale_color_manual(values = getPalette(colorCount)) +
+			theme(legend.position="none")
+		})
+		
+		# Render Expression Plot
+		output$plot_expression <- renderPlot({
+			selected_gate_value <- global$X[,input$visualise_channel]
+			
+			jet.colors <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan",
+							 "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))
 
-		})		
-	}	
+	
+			df <- data.frame(global$coords)	
+			ggplot(df, aes(x=df[,1], y=df[,2], color=selected_gate_value)) + geom_point() +
+			xlab(colnames(global$coords)[1]) +
+			ylab(colnames(global$coords)[2]) +
+			scale_colour_gradientn(colours = jet.colors(7), name="Expression")	
+		})
+
+		# Render Cluster Explorer
+		output$cluster_explorer <- renderPlot({
+			getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+			colorCount <- length(unique(global$g))
+	
+			df <- data.frame(global$coords)
+			df$Gates <- as.factor(global$g)
+			ggplot(df, aes(x=df[,1], y=df[,2], color=Gates)) + geom_point() +
+			xlab(colnames(global$coords)[1]) +
+			ylab(colnames(global$coords)[2]) +
+			scale_color_manual(values = getPalette(colorCount)) +
+			theme(legend.position="none")
+			
+		})
+		
+		# Render Cluster Information text
+		output$cluster_info <- renderPrint({
+	
+			np <- data.frame(nearPoints(as.data.frame(global$coords),
+				   input$plot_click,
+				   xvar = colnames(global$coords)[1],
+				   yvar = colnames(global$coords)[2],
+				   threshold = 10, maxpoints = 1,
+				   addDist = FALSE))
+
+			if (nrow(np)==0){
+				data.frame(np)
+			}else{
+				cluster <- global$g[as.numeric(rownames(np))]
+				members <- sum(global$g == cluster)
+				data.frame(np, Cluster=cluster, Members=members)
+			}
+		})
+	
+	})	
+
+	# Close Server Function		
+	}
+
+# Close APP	
 )
 	runApp(app)
 
