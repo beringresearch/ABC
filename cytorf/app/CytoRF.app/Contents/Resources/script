9a935a3c5a5424a760925a4736684b76ffab570f
Ignat Drozdov <idrozdov@beringresearch.com>
2017-08-23 19:14:47 +0100

    cleaning up changes

---
+++ cytorf/app/CytoRF.app/Contents/Resources/script
@@ -0,0 +1,2 @@
+/usr/local/bin/Rscript -e "library(cytorf); port <- sample(1000:9999, 1); url <- paste0('http://localhost:', port); browseURL(url); cytorf.ui(port)"
+
--- cytorf/platypus.sh
+++
@@ -1,2 +0,0 @@
-/usr/local/bin/Rscript -e "library(cytorf); port <- sample(1000:9999, 1); url <- paste0('open http://localhost:', port); system(url); cytorf.ui(port);"
-
