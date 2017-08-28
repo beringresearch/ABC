16670a1af1e0dea9ee5a6b93afec5a4dc4dea0e2
Ignat Drozdov <idrozdov@beringresearch.com>
2017-08-28 15:51:57 +0100

    change docker image name to public repo

--- cytorf/app/CytoRF.sh
+++ cytorf/app/CytoRF.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-/usr/local/bin/docker run -d -v /Users/:/home/shiny -p 80:3838 cytorf
+/usr/local/bin/docker run --rm -d -v /Users/:/home/shiny -p 80:3838 beringresearch/cytorf
+
 sleep 1
+
 open CytoRF.app 
 
