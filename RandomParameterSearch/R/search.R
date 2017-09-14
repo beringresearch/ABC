5cdaba696dc5c4d4a3d50e8b8fd1c15a0dd2dc41
Ignat Drozdov <idrozdov@beringresearch.com>
2017-09-14 21:51:12 +0100

    appropriate list conversion

--- RandomParameterSearch/R/search.R
+++ RandomParameterSearch/R/search.R
@@ -11,7 +11,7 @@ search <- function(grid, FUN, maximise = FALSE){
   grid_df <- as.data.frame(grid, check.names = FALSE)
 
  capture.output( res <- apply(grid_df, 1, function(x){
-                              params <- list(x)
+                              params <- as.list(x)
                               do.call(FUN, args = params)
           })
   )
