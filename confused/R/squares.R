94b23188962055f8e00e8af6221255756ee5e963
Ignat Drozdov <idrozdov@beringresearch.com>
2017-10-28 11:40:33 +0100

    error checks, axis colours, misclassification visualisations

--- confused/R/squares.R
+++ confused/R/squares.R
@@ -3,10 +3,11 @@
 #' @param predicted   a vector of predicted class probabilities
 #' @param reference   a vector (factor) of classes to be used as the tru results
 #' @param bins        integer indicating number of bins to be used for histograms
+#' @param threshold   numeric positive class threshold. Defaults to 1/nclasses
 #' @import ggplot2
 #' @export
 
-squares <- function(predicted, reference, bins = 10){
+squares <- function(predicted, reference, bins = 10, threshold = 1/length(levels(reference))){
 
   bering.colours <- c("#B1CDE1", "#4E77B1", "#BFDE8E", "#639F3A", "#E49998", "#C12022",
                       "#EDBD73", "#E47E1D", "#C3B1D4", "#603F96",
@@ -16,27 +17,41 @@ squares <- function(predicted, reference, bins = 10){
                       
   classes <- levels(reference)
   
-  df <- data.frame(predicted, Reference = reference, check.names = FALSE)
+  if (length(classes) > 20)
+    stop("This functions doesn't support > 20 classes at this time.")
+  if (ncol(predicted) != length(classes))
+    stop("Number of columns in predicted class probabilities does not match total number of classes.")
+   
+  label <- colnames(predicted)[apply(predicted, 1, which.max)]
+  df <- data.frame(predicted, Reference = reference, Label = label,
+                   check.names = FALSE)
   
   for (n in 1:length(classes)){
     df[df$Reference != classes[n],
        which(colnames(df) == classes[n])] <- NA
   }
 
-  df <- reshape2::melt(df, id.vars = "Reference")
+  m <- reshape2::melt(df, id.vars = c("Reference", "Label"))
+  hline <- data.frame(Group = levels(reference))
 
-  g <- ggplot(df, aes(x=value, fill = Reference)) +
-        geom_histogram(aes(y=..density..), bins = bins, na.rm=TRUE) +
-        coord_flip() +
-        facet_wrap(~Reference, nrow = 1) + theme_bw() +
-        scale_fill_manual(values=bering.colours[1:length(classes)]) +
+  g <- ggplot() +
+        geom_histogram(data = m, aes(x = value, y=..ndensity.., fill = Label),
+                       bins = bins, na.rm=TRUE) +
+        geom_hline(data = m, aes(yintercept = 0, colour = variable, group = variable)) + 
+        geom_vline(xintercept = threshold, linetype = 2, colour = "grey", size = 0.2) +
+        coord_flip() + 
+        scale_fill_manual(values = bering.colours[1:length(classes)]) +
+        scale_colour_manual(values = bering.colours[1:length(classes)]) +
+        facet_wrap(~variable) +
         theme_classic() +
-        theme(axis.line.x = element_line(color="white"),
+        theme(axis.line.x = element_line(colour = "white"),
+              axis.line.y = element_line(colour = "white"),
               axis.ticks.x = element_blank(),
-              axis.text.x = element_text(colour="white"),
+              axis.ticks.y = element_blank(),
+              axis.text.x = element_text(colour = "white"),
               strip.background = element_blank(),
-              legend.position = "none") +
-         ylab("") + xlab("Prediction Score")
+              legend.position = "none") + 
+        ylab("") + xlab("Prediction Score")
    
   return(g)  
 }
