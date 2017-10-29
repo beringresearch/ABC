807b4781ce070d9fbc67d0ea66918808205549f2
Ignat Drozdov <idrozdov@beringresearch.com>
2017-10-29 09:49:54 +0000

    delete repo implemented

--- mlvc/R/ml_delete.R
+++ mlvc/R/ml_delete.R
@@ -1,2 +1,19 @@
 #' Delete model version control history
+#'
+#' @param title   character string of a model repository
+#' @param version character string of the specific model version to be deleted. Not implemented
+#' @import DBI RSQLite
+#' @export
 
+ml_delete <- function(title, version){
+  # Get path to mlvc database
+  HOME <- Sys.getenv("HOME")
+  mlvc_dir <- file.path(HOME, ".mlvc") 
+  
+  mlvc <- dbConnect(SQLite(), file.path(mlvc_dir, "mlvc.sqlite"))
+
+  dbRemoveTable(mlvc, title)
+  
+  dbDisconnect(mlvc) 
+
+}
