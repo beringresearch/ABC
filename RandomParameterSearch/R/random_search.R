e4de0e6d55f999c97c4d6b680dff5ce320cf93de
Ignat Drozdov <idrozdov@beringresearch.com>
2017-11-17 10:55:54 +0000

    remove verbosity param

--- RandomParameterSearch/R/random_search.R
+++ RandomParameterSearch/R/random_search.R
@@ -4,7 +4,6 @@
 #' @param FUN       function to be applied
 #' @param maximise  boolean value indicate weather parameters that maximise a function
 #'                  should be selected. Defaults to FALSE
-#' @param verbose   boolean value that controls output verbosity. Defaults to FALSE.
 #' @importFrom pbapply pbapply
 #' @export
 
@@ -14,8 +13,7 @@ random_search <- function(grid, FUN, maximise = FALSE, verbose = FALSE){
 
   res <- pbapply(grid_df, 1, function(x){
                               args <- as.list(x)
-                              capture.output(value <- do.call(FUN, args = args))
-                              if (verbose) cat("Value: ", value, "\n")
+                              capture.output(value <- do.call(FUN, args = args)) 
                               value
           })
   
