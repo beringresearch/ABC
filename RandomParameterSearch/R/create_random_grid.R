0b3903961a2ff764811b4fd79ff0d08821376f03
Ignat Drozdov <idrozdov@beringresearch.com>
2017-12-14 13:39:50 +0000

    repeat param if the length is 1

--- RandomParameterSearch/R/create_random_grid.R
+++ RandomParameterSearch/R/create_random_grid.R
@@ -8,10 +8,14 @@
 create_random_grid <- function(nrounds = 100, params, seed = 1234){
 
   space <- lapply(params, function(x){
-                    if (class(x) == "integer"){
-                      res <- sample(min(x):max(x), nrounds, replace = TRUE)
-                    } else{
-                      res <- runif(n = nrounds, min = min(x), max = max(x)) 
+                    if (length(x) > 1){
+                      if (class(x) == "integer"){
+                        res <- sample(min(x):max(x), nrounds, replace = TRUE)
+                      } else{
+                        res <- runif(n = nrounds, min = min(x), max = max(x)) 
+                      }
+                    }else{
+                      res <- rep(x, nrounds)
                     }
                     return(res)
                 })
