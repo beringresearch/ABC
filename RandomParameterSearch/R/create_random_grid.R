3043bcb0d57c03e6cc084fe22746301fd52d1df1
Ignat Drozdov <idrozdov@beringresearch.com>
2017-12-16 14:21:30 +0000

    add support for categorical parameters

--- RandomParameterSearch/R/create_random_grid.R
+++ RandomParameterSearch/R/create_random_grid.R
@@ -1,6 +1,6 @@
 #' Create a Random Search Space
 #'
-#' @param nrounds   number of random iterations
+#' @param nrounds   number of random samples from continuous hyperparameters
 #' @param params    a list of parameters with low and high values
 #' @param seed      random seed
 #' @export
@@ -11,15 +11,19 @@ create_random_grid <- function(nrounds = 100, params, seed = 1234){
                     if (length(x) > 1){
                       if (class(x) == "integer"){
                         res <- sample(min(x):max(x), nrounds, replace = TRUE)
-                      } else{
+                      } else if(typeof(x) == "double"){
                         res <- runif(n = nrounds, min = min(x), max = max(x)) 
-                      }
+                      } else if(typeof(x) == "character"){
+                        res <- x
+                      } 
                     }else{
-                      res <- rep(x, nrounds)
+                      res <- x 
                     }
                     return(res)
                 })
 
-  return(space)
+  res <- expand.grid(space, stringsAsFactors = FALSE)
+
+  return(res)
 
 }
--- RandomParameterSearch/R/random_search.R
+++ RandomParameterSearch/R/random_search.R
@@ -4,20 +4,18 @@
 #' @param FUN       function to be applied
 #' @param maximise  boolean value indicate weather parameters that maximise a function
 #'                  should be selected. Defaults to FALSE
-#' @importFrom pbapply pbapply
+#' @importFrom pbapply pblapply
 #' @export
 
 random_search <- function(grid, FUN, maximise = FALSE){
-
-  grid_df <- as.data.frame(grid, check.names = FALSE)
-
-  res <- pbapply(grid_df, 1, function(x){
-                              args <- as.list(x)
-                              capture.output(value <- do.call(FUN, args = args)) 
+  
+  gridl <- split(grid, seq(nrow(grid)))
+  res <- pblapply(gridl, function(x){
+                              capture.output(value <- do.call(FUN, args = x)) 
                               value
           })
   
-  output <- data.frame(grid_df, .output = res)
+  output <- data.frame(grid, .output = unlist(res))
   return(output)
 
 }
